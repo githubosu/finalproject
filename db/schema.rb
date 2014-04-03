56074a9138c775507eaba7aeacc39f40e7d1419c
tnh920@gmail.com <tnh920@gmail.com>
2014-04-02 23:23:06 -0400

    added events database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140401221635) do
+ActiveRecord::Schema.define(version: 20140403031735) do
+
+  create_table "events", force: true do |t|
+    t.string   "title"
+    t.string   "color"
+    t.datetime "start"
+    t.datetime "end"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
